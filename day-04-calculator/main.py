2566d825a669dfa618d2ec62e0f5c4cb2a56bb1b
sdhaval97 <sdhaval97@gmail.com>
2025-08-22 01:00:00 +0530

    added clear screen and summation function

---
+++ day-04-calculator/main.py
@@ -0,0 +1,9 @@
+import os
+
+def clear_screen():
+    """Clears the console screen"""
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def add(n1, n2):
+    """Returns the sum of 2 numbers"""
+    return n1 + n2
