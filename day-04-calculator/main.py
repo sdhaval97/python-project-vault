66e924fb5963026b2af2f88c6641259c0eb18daa
sdhaval97 <sdhaval97@gmail.com>
2025-08-24 00:16:23 +0530

    added subtraction and multiplication functions

--- day-04-calculator/main.py
+++ day-04-calculator/main.py
@@ -7,3 +7,11 @@ def clear_screen():
 def add(n1, n2):
     """Returns the sum of 2 numbers"""
     return n1 + n2
+
+def subtract(n1, n2):
+    """Returns the difference between the 2 numbers"""
+    return n1 - n2
+
+def multiply(n1, n2):
+    """Returns the product of 2 numbers"""
+    return n1 * n2
