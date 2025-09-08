d8578f7db62a9e5d455fbf7aca83b9c14b9d3bef
sdhaval97 <sdhaval97@gmail.com>
2025-09-08 23:52:09 +0530

    added load tasks functionality

--- day-07-to-do-list/main.py
+++ day-07-to-do-list/main.py
@@ -6,4 +6,16 @@ TASKS_FILE = 'tasks.txt'
 def clear_screen():
     """Clear the console screen"""
     os.system('cls' if os.name == 'nt' else 'clear')
+
+def load_tasks():
+    """Loads tasks from the tasks file. Creates the file if doesn't exist"""
+    if not os.path.exists(TASKS_FILE):
+        return[]
+    try:
+        with open(TASKS_FILE, 'r', encoding='utf-8') as f:
+            tasks = [line.strip() for line in f.readlines()]
+        return tasks
+    except Exception as e:
+        print("An error occured while loading tasks: {e}")
+        return []
     
